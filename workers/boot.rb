837ba6c4895c3e437f7a2e355fe5ea6a73deeea5
Charlie Moseley <charlie@rofl.io>
2013-04-26 18:50:16 -0700

    Creates a worker to create new calendars.

--- workers/boot.rb
+++ workers/boot.rb
@@ -2,12 +2,26 @@
 require 'sidekiq'
 require 'redis'
 
+# Load up the libraries
+require_relative '../lib/echidna/echidna'
+require_relative '../lib/gcalendar/gcalendar'
+
 # Load up the models
 require_relative '../models/boot'
 
 # Load up the workers
 require_relative 'message/test'
 require_relative 'gcal/create_upcoming_calendar'
+
+# Setup the defaults for our Oauth2 connections
+GCalendar::Config.client_id     = ENV['GOOGLE_KEY']
+GCalendar::Config.client_secret = ENV['GOOGLE_SECRET']
+GCalendar::Config.callback_token_refreshed = ->(provider, user_uid, response) do
+  provider = provider + "_oauth2"
+  account = Account.find_by_provider_and_uid(provider, user_uid)
+  account.token = response.access_token
+  account.save
+end
 
 # Set the base class with connection
 class Workers
--- workers/gcal/create_upcoming_calendar.rb
+++ workers/gcal/create_upcoming_calendar.rb
@@ -1,19 +1,88 @@
+# This worker checks whether or not EventBox's upcoming calendar exists or
+# not and appriopately handles the situation.
+
 class Workers
   class GCal
     class CreateUpcomingCalendar
       def perform(account_id)
-        account            = Account.find(account_id)
-        save_updated_token = ->(response, refresh_token) {
-          account = Account.find_by_refresh_token refresh_token
-          account.token         = response.access_token
-          account.refresh_token = response.refresh_token
-          account.save
-        }
+        account = Account.includes(:user).find(account_id)
 
-        calendar = GCalendar::Calendar.new access_token:  account.token,
-                                           refresh_token: account.refresh_token
-        calendar.summary = "Upcoming Events (Eventbox.io)"
+        # We don't have a calendar for this user
+        if account.calendars.empty?
+          external_calendar = create_external_calendar account
+          create_internal_calendar account, external_calendar
+          return true
+        end
+
+        # We do have a calendar on our internal record
+        internal_calendar = account.calendars.select{ |c| c.provider == "google" }.first
+        external_calendar = find_external_calendar account, internal_calendar.calendar_id
+
+        # Something went wrong
+        if external_calendar.kind_of? Echidna::Error
+          if external_calendar.body.error.code == 404
+            # The calendar just couldn't be found, so let's recreate it.
+            external_calendar = create_external_calendar account
+            update_internal_calendar account, internal_calendar, external_calendar
+            return true
+          else
+            #  Raise an error so Sidekiq can try again later
+            raise external_calendar
+          end
+        end
+
+        # If it wasn't an error, then we know everything is good.  The user is
+        # allowed to customize the calendar as they see fit, we just update the
+        # values of our internal version to know any changes they made in case
+        # we need to recreate it.
+
+        # WE WILL IMPLEMENT THIS AFTER GETTING ETAGS WORKING AS TO NOT MAKE
+        # EXCESSIVE QUERIES
+      end
+
+      def create_external_calendar(account)
+        calendar = GCalendar::Calendar.new \
+                     access_token:  account.token,
+                     refresh_token: account.refresh_token,
+                     user_uid:      account.uid
+        calendar.summary = "Upcoming Events (EventBox.io)"
         calendar.save
+
+        calendar
+      end
+
+      def find_external_calendar(account, calendar_id)
+        GCalendar::Calendar.find calendar_id,
+                                 access_token:  account.token,
+                                 refresh_token: account.refresh_token,
+                                 user_uid:      account.uid
+      end
+
+      def create_internal_calendar(account, external_calendar)
+        internal_calendar = Calendar.new
+        internal_calendar.user        = account.user
+        internal_calendar.account     = account
+        internal_calendar.provider    = "google"
+        internal_calendar.purpose     = "upcoming"
+        internal_calendar.calendar_id = external_calendar.id
+        internal_calendar.etag        = external_calendar.etag
+        internal_calendar.raw         = external_calendar.to_json
+        internal_calendar.save
+
+        internal_calendar
+      end
+
+      def update_internal_calendar(account, internal_calendar, external_calendar)
+        internal_calendar.user        = account.user
+        internal_calendar.account     = account
+        internal_calendar.provider    = "google"
+        internal_calendar.purpose     = "upcoming"
+        internal_calendar.calendar_id = external_calendar.id
+        internal_calendar.etag        = external_calendar.etag
+        internal_calendar.raw         = external_calendar.to_json
+        internal_calendar.save
+
+        internal_calendar
       end
     end
   end
