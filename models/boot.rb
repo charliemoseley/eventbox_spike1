76f6daac936ff4327bcfe4ea59ae68c6c69503de
Charlie Moseley <charlie@rofl.io>
2013-04-27 13:07:01 -0700

    Adds a postgres config for prod.

--- models/boot.rb
+++ models/boot.rb
@@ -1,10 +1,24 @@
-require 'sqlite3'
 require 'active_record'
+require 'uri'
 
-ActiveRecord::Base.establish_connection(
-  adapter: 'sqlite3',
-  database: 'db/eventbox.db'
-)
+unless [:production, :staging].include? ENV['RACK_ENV']
+  ActiveRecord::Base.establish_connection(
+    adapter: 'sqlite3',
+    database: 'db/eventbox.db'
+  )
+else # Prod and Staging
+  db = URI.parse ENV['DATABASE_URL']
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :port     => db.port,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
 
 # Load up all the models
 require_relative 'user'
