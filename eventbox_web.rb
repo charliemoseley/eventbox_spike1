57926b0b224e641e0c357638e9a204cbc1ecc65a
Charlie Moseley <charlie@rofl.io>
2013-04-28 14:53:51 -0700

    Created a protected_page helper + reorganized the code in the sinatra app.

--- eventbox_web.rb
+++ eventbox_web.rb
@@ -2,33 +2,26 @@ require_relative 'config/models'
 require_relative 'config/workers'
 require_relative 'config/sinatra'
 
-class EventBoxWeb < Sinatra::Base
-  before do
-    pass if public_pages
-    login_required
-  end
-
-  def public_pages
-    return true if request.path_info == '/'
-    p = %w[auth error logout message wait-list]
-    p.include? request.path_info.split('/')[1]
-  end
-
-  def login_required
-    unless current_user
-      flash[:error] = "You need to be logged in to access this page."
-      redirect '/'
-    end
-  end
-  
+class EventBoxWeb < Sinatra::Base  
   get '/' do
     erb :'pages/index'
   end
-
+  
   post '/wait-list' do
     puts "Success!"
   end
   
+  get '/dashboard' do
+    protected_page
+    erb :'pages/dashboard'
+  end
+  
+  # Test Routes
+  get '/message' do
+    Worker::Message::Test.perform_async("Hi Sidekiq")
+  end
+  
+  # Authentication Routes
   get '/auth/google_oauth2/callback' do
     user = User.login request.env["omniauth.auth"]
     
@@ -46,26 +39,15 @@ class EventBoxWeb < Sinatra::Base
     account = current_user.add_or_update_account request.env["omniauth.auth"]
     redirect '/dashboard'
   end
-  
-  get '/dashboard' do
-    erb :'pages/dashboard'
-  end
 
-  get '/test' do
-    puts "Hi, test output"
-  end
-  
-  get '/message' do
-    Worker::Message::Test.perform_async("Hi Sidekiq")
-  end
-  
-  get '/error' do
-    'Uh oh! Something went wrong.'
-  end
-  
   get '/logout' do
     session[:user_id] = nil
     flash[:notice] = "You have been logged out."
     redirect '/'
+  end
+  
+  # Error Routes
+  get '/error' do
+    'Uh oh! Something went wrong.'
   end
 end
--- helpers/application.rb
+++ helpers/application.rb
@@ -3,5 +3,12 @@ class EventBoxWeb < Sinatra::Base
     def current_user
       @current_user ||= User.find session[:user_id] if session[:user_id]
     end
+
+    def protected_page
+      unless current_user
+        flash[:error] = "You need to be logged in to access this page."
+        redirect '/'
+      end
+    end
   end
 end
